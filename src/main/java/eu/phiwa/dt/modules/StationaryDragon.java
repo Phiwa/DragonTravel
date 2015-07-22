a0fd9dfb6639707ef88d3237f311cb2f2f11709b
Ollie <olliehayes96@gmail.com>
2015-07-22 15:55:52 +0100

    - Updated pom.xml to allow everyone to easily add their own source JARs for craftbukkit. - Reset waypoint markers one tick after loading the chunk to allow for some latency. - Let's try an invisible slowness potion for slowing down dragon wings - Added pitch control for the dragon - Added fallback update for yaw and pitch - Fixed stationary dragon wing speed to the same method as traveling dragons

--- src/main/java/eu/phiwa/dt/modules/StationaryDragon.java
+++ src/main/java/eu/phiwa/dt/modules/StationaryDragon.java
@@ -3,9 +3,12 @@ package eu.phiwa.dt.modules;
 import eu.phiwa.dt.DragonTravelMain;
 import eu.phiwa.dt.RyeDragon;
 import net.minecraft.server.v1_8_R3.World;
-
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class StationaryDragon {
 
@@ -14,8 +17,17 @@ public class StationaryDragon {
 	 */
 	public static void createStatDragon(Player player) {
 		World notchWorld = ((CraftWorld) player.getWorld()).getHandle();
-		RyeDragon dragon = new RyeDragon(player.getLocation(), notchWorld);
+		final RyeDragon dragon = new RyeDragon(player.getLocation(), notchWorld);
 		notchWorld.addEntity(dragon);
+        ((LivingEntity)dragon.getEntity()).damage(2);
+		Bukkit.getScheduler().runTaskLater(DragonTravelMain.plugin, new Runnable() {
+			@Override
+			public void run() {
+                ((LivingEntity)dragon.getEntity()).damage(2);
+                ((LivingEntity)dragon.getEntity()).setHealth(dragon.getMaxHealth());
+                ((LivingEntity)dragon.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Integer.MAX_VALUE, 1, false));
+			}
+		}, 2L);
 		player.sendMessage(DragonTravelMain.messagesHandler.getMessage("Messages.General.Successful.AddedStatDragon"));
 	}
 }
