b2c19f38d38c14b7f6a22c5bc1295a70fe628503
YAMSAdmin <59032765+YAMSAdmin@users.noreply.github.com>
2019-12-22 13:11:03 +0100

    Indent & debug

--- src/main/java/eu/phiwa/dragontravel/nms/CompatibilityUtils.java
+++ src/main/java/eu/phiwa/dragontravel/nms/CompatibilityUtils.java
@@ -14,23 +14,24 @@ public class CompatibilityUtils {
         "SIGN_POST",
         "LEGACY_WALL_SIGN",
         "LEGACY_SIGN_POST",
-		"ACACIA_SIGN",
-		"ACACIA_WALL_SIGN",
-		"BIRCH_SIGN",
-		"BIRCH_WALL_SIGN",
-		"DARK_OAK_SIGN",
-		"DARK_OAK_WALL_SIGN",
-		"JUNGLE_SIGN",
-		"JUNGLE_WALL_SIGN",
-		"OAK_SIGN",
-		"OAK_WALL_SIGN",
-		"SPRUCE_SIGN",
-		"SPRUCE_WALL_SIGN"
+        "ACACIA_SIGN",
+        "ACACIA_WALL_SIGN",
+        "BIRCH_SIGN",
+        "BIRCH_WALL_SIGN",
+        "DARK_OAK_SIGN",
+        "DARK_OAK_WALL_SIGN",
+        "JUNGLE_SIGN",
+        "JUNGLE_WALL_SIGN",
+        "OAK_SIGN",
+        "OAK_WALL_SIGN",
+        "SPRUCE_SIGN",
+        "SPRUCE_WALL_SIGN"
     );
 
     public static boolean typeIsSign(Material t) {
         // Nasty trick to maintain compatibility with newer versions where the Material enum's fields might change
         //   e.g. SIGN_POST => LEGACY_SIGN_POST in CB v1_13_R1
+        System.out.println( t ); // debug
         return signTypes.contains(t.name());
     }
 }
